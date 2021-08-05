ef2fea7bed045a3af65bfad201868c961b9c1ef1
Chris Harris <chris.harris@kitware.com>
2021-08-05 15:19:59 -0400

    Add auth for websockets

--- backend/app/app/api/api_v1/endpoints/notifications.py
+++ backend/app/app/api/api_v1/endpoints/notifications.py
@@ -1,9 +1,12 @@
 import asyncio
+from contextlib import contextmanager
 from typing import Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.exceptions import HTTPException
 from starlette.endpoints import WebSocketEndpoint
 
+from app.api.deps import get_current_user, get_db
 from app.kafka import consumer
 
 router = APIRouter()
@@ -13,16 +16,31 @@ from fastapi import WebSocket
 
 @router.websocket_route("/notifications")
 class WebsocketConsumer(WebSocketEndpoint):
-    async def on_connect(self, websocket: WebSocket) -> None:
-        self.websocket = websocket
-        await self.websocket.accept()
+    relay_task = None
+    consumer = None
 
-        self.consumer = await consumer.create()
-        self.relay_task = asyncio.create_task(self.relay_events())
+    async def on_connect(self, websocket: WebSocket) -> None:
+        try:
+            self.websocket = websocket
+            await self.websocket.accept()
+
+            with contextmanager(get_db)() as db:
+                await get_current_user(db, websocket.query_params.get("token"))
+
+            self.consumer = await consumer.create()
+            self.relay_task = asyncio.create_task(self.relay_events())
+        except HTTPException as hex:
+            if hex.status_code == status.HTTP_401_UNAUTHORIZED:
+                await self.websocket.send_text(hex.detail)
+                await self.websocket.close()
+            else:
+                raise
 
     async def on_disconnect(self, websocket: WebSocket, close_code: int) -> None:
-        self.relay_task.cancel()
-        await self.consumer.stop()
+        if self.relay_task:
+            self.relay_task.cancel()
+        if self.consumer:
+            await self.consumer.stop()
 
     async def on_receive(self, websocket: WebSocket, data: Any) -> None:
         # For now do nothing

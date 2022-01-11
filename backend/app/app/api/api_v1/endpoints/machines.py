df8ddc00d3963e7fa61cc3fbb890d226d72e6001
Chris Harris <chris.harris@kitware.com>
2022-01-11 12:07:51 -0500

    Add endpoint to fetch machine specific configuration

--- backend/app/app/api/api_v1/endpoints/machines.py
+++ backend/app/app/api/api_v1/endpoints/machines.py
@@ -1,9 +1,11 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
-from app.api.deps import oauth2_password_bearer_or_api_key
+from app.api.deps import oauth2_password_bearer_or_api_key, get_api_key
 from app.core.config import settings
+from app import schemas
+
 
 router = APIRouter()
 
@@ -17,3 +19,16 @@ def read_machines():
     machines = [m.name for m in settings.MACHINES]
 
     return machines
+
+@router.get(
+    "/{name}",
+    response_model=schemas.Machine,
+    dependencies=[Depends(get_api_key)],
+)
+def read_machine(name: str):
+
+    for m in settings.MACHINES:
+        if m.name == name:
+            return m
+
+    raise HTTPException(status_code=404, detail="Machine not found")
--- backend/app/app/schemas/machine.py
+++ backend/app/app/schemas/machine.py
@@ -1,5 +1,14 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class Machine(BaseModel):
     name: str
+    account: str
+    qos: str
+    nodes: int
+    constraint: str
+    ntasks_per_node: Optional[str]
+    cpus_per_task: int
+    cpu_bind: Optional[str]
+    bbcp_dest_dir: str

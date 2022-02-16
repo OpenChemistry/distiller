38d757ca6b906c2b071f55e75de30e274f632080
Chris Harris <chris.harris@kitware.com>
2022-02-16 09:39:09 -0500

    Fixup styling of machine select

--- frontend/distiller/src/components/machine-option.tsx
+++ frontend/distiller/src/components/machine-option.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 import { Chip, MenuItem } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
 
 import { Machine } from '../types';
 import { canRunJobs, statusColor } from '../utils/machine';
+
+const useStyles = makeStyles((_theme) => ({
+  menu: {
+    justifyContent: 'space-between',
+    display: 'flex',
+    flexGrow: 1,
+  },
+}));
 
 function statusIcon(machine: Machine) {
   const { status, notes } = machine;
@@ -23,13 +32,17 @@ function statusIcon(machine: Machine) {
 }
 
 const MachineOptionComponent = (machine: Machine) => {
+  const classes = useStyles();
+
   return (
     <MenuItem
       value={machine.name}
       key={machine.name}
       disabled={!canRunJobs(machine.status)}
     >
-      {machine.name} <span style={{ flexGrow: 1 }}></span> {statusIcon(machine)}
+      <div className={classes.menu}>
+        <div>{machine.name}</div> {statusIcon(machine)}
+      </div>
     </MenuItem>
   );
 };

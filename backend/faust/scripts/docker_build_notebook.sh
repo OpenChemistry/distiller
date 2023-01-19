c5c958d83c9a7f97e2da38724e53ffc66b7a5a76
Chris Harris <cjh@lbl.gov>
2023-01-19 16:48:45 -0500

    Pass all args to script

--- backend/faust/scripts/docker_build_notebook.sh
+++ backend/faust/scripts/docker_build_notebook.sh
@@ -2,4 +2,4 @@
 
 TAG=`git log -1 --pretty=%h`
 
-docker build --build-arg WORKER=notebook -t openchemistry/distiller-faust-notebook:$TAG -f ../Dockerfile.worker ../
+docker build  --build-arg WORKER=notebook -t openchemistry/distiller-faust-notebook:$TAG -f ../Dockerfile.notebook_worker ../ $@

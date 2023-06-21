e210acd08a69d0d60ebd09848076964a4273b5be
Sam Welborn <swelborn@lbl.gov>
2023-06-21 15:24:30 -0400

    backend, db: add submit to job table

--- backend/app/app/models/job.py
+++ backend/app/app/models/job.py
@@ -1,5 +1,4 @@
-from sqlalchemy import (JSON, Column, Enum, ForeignKey, Integer, Interval,
-                        String)
+from sqlalchemy import JSON, Column, Enum, Integer, Interval, String, DateTime
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
@@ -17,5 +16,6 @@ class Job(Base):
     output = Column(String, nullable=True)
     elapsed = Column(Interval, nullable=True)
     machine = Column(String, nullable=False)
+    submit = Column(DateTime(timezone=True), nullable=True, index=True)
     notes = Column(String, nullable=True)
     scans = relationship("Scan", secondary=scan_job_table, back_populates="jobs")
